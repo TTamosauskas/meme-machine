34eb9f8ace8d9d1b7a1779aa8089585a5efe4250
TTamosauskas <thiago.gt@gmail.com>
2020-01-29 07:12:44 -0300

    fun: still playing with JSX

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,33 +1,38 @@
 import React from 'react';
 
-function MyInfo(){
+function MyInfo() {
+  const firstName = 'Bob';
+  const lastName = 'Dobbs';
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
 
-const firstName ="Bob"
-const lastName = "Dobbs"
-const date = new Date()
-const hours = date.getHours()
-let timeOfDay
+  if (hours < 12) {
+    timeOfDay = 'um bom dia';
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = 'uma boa tarde';
+  } else {
+    timeOfDay = 'uma boa noite';
+  }
 
-if (hours < 12) {
-  timeOfDay = "um bom dia"
-}else if (hours >= 12 && hours < 17) {
-  timeOfDay = "uma boa tarde"
-} else {
-  timeOfDay = "uma boa noite"
-}
+  const styles = {
+    color: '#ff9c88',
+    backgroundColor: 'red',
+  };
 
   return (
     <div>
       <h1>Bem vindo {`${firstName} ${lastName}`}!</h1>
-  <p>Tenha {timeOfDay}</p>
-    <p>Agora são {date.getHours() % 12} horas</p>
-  <ul>
-    <li>parque</li>
-    <li>shopping</li>
-    <li>circo</li>
-    <li>praia</li>
-    </ul>
-    </div> 
-  )}
-  
-  export default MyInfo;
+      <p>Tenha {timeOfDay}</p>
+      <p style={styles}>Agora são {date.getHours() % 12} horas</p>
+      <ul>
+        <li>parque</li>
+        <li>shopping</li>
+        <li>circo</li>
+        <li>praia</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;
