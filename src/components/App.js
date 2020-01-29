4c446962d248d2eb275f8a6d4066bb8702d31db6
TTamosauskas <thiago.gt@gmail.com>
2020-01-29 07:03:02 -0300

    fun: just playing with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./Header"
 import NavBar from "./NavBar"
+import MyInfo from "./MyInfo"
 import MainContent from "./MainContent"
 import Footer from "./Footer"
 
@@ -9,6 +10,7 @@ function App() {
     <>
 <Header />
 <NavBar />
+<MyInfo />
 <MainContent />
 <Footer />
     </>
--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 function MyInfo(){
+
+const firstName ="Bob"
+const lastName = "Dobbs"
+const date = new Date()
+const hours = date.getHours()
+let timeOfDay
+
+if (hours < 12) {
+  timeOfDay = "um bom dia"
+}else if (hours >= 12 && hours < 17) {
+  timeOfDay = "uma boa tarde"
+} else {
+  timeOfDay = "uma boa noite"
+}
+
   return (
     <div>
-      <h1>Nome Completo</h1>
-      <p>lorem ipsum sicutt ameti</p>
+      <h1>Bem vindo {`${firstName} ${lastName}`}!</h1>
+  <p>Tenha {timeOfDay}</p>
+    <p>Agora são {date.getHours() % 12} horas</p>
   <ul>
     <li>parque</li>
     <li>shopping</li>
