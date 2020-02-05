82a03a13032aa697504aa408cae113f67353257e
TTamosauskas <thiago.gt@gmail.com>
2020-02-05 06:58:16 -0300

    feat: a simples loading conditional

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 
+// https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/fetch
+// https://swapi.com
+// https://medium.com/javascrip-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+
 class Fetch extends Component {
   constructor() {
     super();
     this.state = {
+      loading: false,
       character: {},
     };
   }
 
   componentDidMount() {
+    this.setState({ loading: true });
     fetch('https://swapi.co/api/people/1/')
       .then(response => response.json())
       .then(data => {
         this.setState({ character: data });
+        this.setState({ loading: false });
       });
   }
 
   render() {
-    return <div>{this.state.character.name}</div>;
+    const text = this.state.loading ? 'loading...' : this.state.character.name;
+    return <div>Bring to you by {text}</div>;
   }
 }
 
