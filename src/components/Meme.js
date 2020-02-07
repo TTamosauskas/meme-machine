e95221fef18c18012bd5f25d4de6955d9f7641da
TTamosauskas <thiago.gt@gmail.com>
2020-02-07 06:34:09 -0300

    feat: fetching a array of memes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import MemeData from './MemeData';
 
 class Meme extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      topText: '',
+      bottomText: '',
+      randomImage: 'http://i.imgflip.com/ibij.jpg',
+      allMemeImages: MemeData,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    fetch(MemeData)
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data;
+        console.log(memes[0]);
+        this.setState({ allMemeImages: memes });
+      });
   }
 
   render() {
